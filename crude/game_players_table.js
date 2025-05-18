f4f7e62f34eeb384367c64d67be031da77ba944d
adler3d <adler3d@gmail.com>
2025-05-18 18:44:09 +0300

    Update game_players_table.js

--- crude/game_players_table.js
+++ crude/game_players_table.js
@@ -1,3 +1,6 @@
 if(typeof game_table=='undefined')game_table=[];
-game_table.push({user:POST.user,ms:POST.ms});
-return jstable(game_table);//html_utf8('<body>'+os.uptime()+'</body>');
+//game_table=[];
+if('user' in qp)game_table.push({user:POST.user?POST.user:"nope",ms:POST.ms?POST.ms:200,date:getDateTime()});
+//fs.writeFileSync("",POST.code);
+// 
+return ('user' in qp)?JSON.stringify(game_table):jstable(game_table);//html_utf8('<body>'+os.uptime()+'</body>');
