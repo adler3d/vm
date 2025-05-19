5fa51a48328bac2ec976ee8f3ca511f9971a71e3
adler3d <adler3d@gmail.com>
2025-05-19 18:45:26 +0300

    Update game_players_table.js

--- crude/game_players_table.js
+++ crude/game_players_table.js
@@ -6,8 +6,10 @@ if(typeof game_table=='undefined'){
 }
 let ok=fs.existsSync(fn);
 if(!ok&&game_table.length>0){fs.writeFileSync(fn,game_table.map(e=>json(e)).join("\n"));ok=true;}
-if('user' in qp){
-  let rec={'#':0,user:POST.user?POST.user:"nope",sec:POST.sec?POST.sec*1.0:200,date:getDateTime(),seed:POST.seed*1,game:POST.game};
+let isInt=(n)=>Number(n)===n&&n%1===0;
+let isFloat=(n)=>(Number(n)===n&&n%1!==0)||isInt(n);
+if(('user' in qp)&&('sec' in qp)&&('date' in qp)&&('seed' in qp)&&('game' in qp)&&isInt(1*POST.seed)&&isFloat(1.0*POST.sec)){
+  let rec={'#':0,user:POST.user?POST.user:"nope",sec:POST.sec*1.0,date:getDateTime(),seed:POST.seed*1,game:POST.game};
   game_table.push(rec);
   fs.appendFileSync(fn,(ok?"\n":"")+json(rec));
   return "["+getDateTime()+"] ok";
